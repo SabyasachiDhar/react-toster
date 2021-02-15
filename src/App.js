c8cb6aa519bfd9c1f7b7931d48b20ca3ece10b43
SabyasachiDhar <dhar.sabyasachi.vi@gmail.com>
2021-02-15 15:14:52 +0530

    1 file modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./style.css";
 
+import FormOne from "./components/formOne.js";
+import FormTwo from "./components/formTwo.js";
+import Main from "./components/main.js";
+import SideBar from "./components/sidebar.js";
+
 export default function App() {
   return (
     <div>
---
+++ src/components/formOne.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const FormOne = () => {
+  return (
+    <div>
+      Form One
+    </div>
+  )
+}
+
+export default FormOne;
